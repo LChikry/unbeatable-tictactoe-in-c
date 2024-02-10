f34b7cbbad0f8795ba3d9a6dfdce6cf3e5802e4d
HChikry <hc4contact@gmail.com>
2024-02-10 20:36:43 +0100

    Fixing the unit test for unbeatable mode

--- test/unit/unbeatable_mode/unbeatable_mode_test.c
+++ test/unit/unbeatable_mode/unbeatable_mode_test.c
@@ -6,6 +6,8 @@
 
 #include "../../../include/common/common.h"
 #include "../../../include/computer_turn/computer_turn.h"
+#include "../../../include/computer_turn/normal_mode.h"
+#include "../../../include/computer_turn/unbeatable_mode.h"
 #include "../../../include/game_play/game_checkers.h"
 #include "../../../include/game_play/game_play.h"
 
@@ -55,17 +57,15 @@ void MarkUserMove(char *board, char place, const char user_playing_symbol) {
   }
 }
 
-void ComputerTurnSimulator(char *board, int number_of_turns,
-                           int *playing_algorithm_used) {
-  int board_place_number = 0;
-
+int ComputerTurnSimulator(char *board, int number_of_turns,
+                          int *playing_algorithm_used) {
   if (PLAYING_EASY_MODE == *playing_algorithm_used) {
-    board_place_number = EasyMode(board);
+    return EasyMode(board);
   }
 
   // Normal Mode used also when the unbeatable algorithm can't win
   if (PLAYING_NORMAL_MODE == *playing_algorithm_used) {
-    board_place_number = NormalMode(board, number_of_turns);
+    return NormalMode(board, number_of_turns);
   }
 
   // Finding The Most Optimal and Unbeatable Algorithm
@@ -74,26 +74,25 @@ void ComputerTurnSimulator(char *board, int number_of_turns,
   }
 
   if (PLAYING_FIRST_ALGO == *playing_algorithm_used) {
-    board_place_number = StartingFirstWinningAlgorithm(board, number_of_turns,
-                                                       playing_algorithm_used);
+    return StartingFirstWinningAlgorithm(board, number_of_turns,
+                                         playing_algorithm_used);
   }
 
   if (PLAYING_SECOND_CORNER == *playing_algorithm_used) {
-    board_place_number = StartingSecondWithEmptyCenterAndCornerSquares(
+    return StartingSecondWithEmptyCenterAndCornerSquares(
         board, number_of_turns, playing_algorithm_used);
   }
 
   if (PLAYING_SECOND_MIDDLE == *playing_algorithm_used) {
-    board_place_number =
-        StartingSecondWithEmptyCenterAndMiddleSquares(board, number_of_turns);
+    return StartingSecondWithEmptyCenterAndMiddleSquares(board,
+                                                         number_of_turns);
   }
 
   if (PLAYING_SECOND_CENTER == *playing_algorithm_used) {
-    board_place_number =
-        StartingSecondWithMarkedCenterSquare(board, number_of_turns);
+    return StartingSecondWithMarkedCenterSquare(board, number_of_turns);
   }
 
-  puts("Something went Wrong in ComputerTurn function");
+  puts("Something went Wrong in ComputerTurnSimulator function");
   exit(1);
 }
 
@@ -124,8 +123,8 @@ bool UnbeatableModeGameSimulation(FILE *instructions, FILE *results,
       i++;
 
     } else {
-      ComputerTurnSimulator((char *)board, number_of_turns,
-                            &playing_algorithm_used);
+      (void)ComputerTurnSimulator((char *)board, number_of_turns,
+                                  &playing_algorithm_used);
 
       should_user_play = true;
     }
