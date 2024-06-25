e38a28f332bd281d54f199502efa4ae5d0768aa5
HChikry <hc4contact@gmail.com>
2024-06-25 16:26:51 +0100

    fixing bugs in checking who won horizontally and vertically in multiple player mode

--- src/friends_gameplay/multiple_player_game_checkers.c
+++ src/friends_gameplay/multiple_player_game_checkers.c
@@ -11,45 +11,49 @@ static int IsSymbolOccurredXTimesAdvanced(char *board,
                                           const char playing_symbol,
                                           const int x_times,
                                           const int direction) {
-  int number_of_squares = (number_of_players + 1) * (number_of_players + 1);
   int symbol_occurrence = 0;
+  const int row_and_column_size = number_of_players + 1;
 
   // Checking Horizontally
   if (direction == HORIZONTAL_DIRECTION) {
-    for (int i = 0; i < number_of_players + 1; i++) {
-      symbol_occurrence = 0;
+    for (int i = 0; i < row_and_column_size - 2; ++i) {
+      for (int j = 0; j < row_and_column_size - 2; ++j) {
+        symbol_occurrence = 0;
 
-      for (int j = 0; j < number_of_players + 1; j++) {
-        if (IsPlaceTakenByX(board, j + (i * (number_of_players + 1)) + 1,
-                            playing_symbol)) {
-          symbol_occurrence++;
+        for (int k = 0; k < x_times; ++k) {
+          if (IsPlaceTakenByX(board, j + (i * row_and_column_size) + 1 + k,
+                              playing_symbol)) {
+            ++symbol_occurrence;
+          }
         }
-      }
 
-      if (symbol_occurrence == x_times) return i + 1;  // column number
+        if (symbol_occurrence == x_times) return i + 1;  // column number
+      }
     }
-    return 0;
+    return DRAW_GAME;
   }
 
   // Checking Vertically
   if (direction == VERTICAL_DIRECTION) {
-    for (int i = 0; i < number_of_players + 1; i++) {
-      symbol_occurrence = 0;
+    for (int i = 0; i < row_and_column_size - 2; ++i) {
+      for (int j = 0; j < row_and_column_size - 2; ++j) {
+        symbol_occurrence = 0;
 
-      for (int j = 0; j < number_of_players + 1; j++) {
-        if (IsPlaceTakenByX(board, i + (j * (number_of_players + 1)) + 1,
-                            playing_symbol)) {
-          symbol_occurrence++;
+        for (int k = 0; k < x_times; ++k) {
+          if (IsPlaceTakenByX(
+                  board,
+                  i + (j * row_and_column_size) + 1 + (k * row_and_column_size),
+                  playing_symbol)) {
+            ++symbol_occurrence;
+          }
         }
-      }
 
-      if (symbol_occurrence == x_times) return i + 1;  // row number
+        if (symbol_occurrence == x_times) return i + 1;  // column number
+      }
     }
-    return 0;
+    return DRAW_GAME;
   }
 
-  //! creating a good strategy to check the X shape
-  int row_and_column_size = number_of_players + 1;
   int up_down_jump_length = row_and_column_size + 1;
   int down_up_jump_length = row_and_column_size - 1;
 
