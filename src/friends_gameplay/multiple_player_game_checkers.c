64331238217707e73528ff843076f494399722eb
HChikry <hc4contact@gmail.com>
2024-06-30 15:56:52 +0100

    refactoring the winner checking algorithm

--- src/friends_gameplay/multiple_player_game_checkers.c
+++ src/friends_gameplay/multiple_player_game_checkers.c
@@ -6,96 +6,93 @@
 #include "../../include/common/common.h"
 #include "../../include/game_play/game_checkers.h"
 
+static int horizontal_checking(char *board, const char playing_symbol,
+                               int board_size, int i, int j, int k) {
+  if (IsPlaceTakenByX(board, j + (i * board_size) + 1 + k, playing_symbol)) {
+    return 1;
+  }
+
+  return 0;
+}
+
+static int vertical_checking(char *board, const char playing_symbol,
+                             int board_size, int i, int j, int k) {
+  if (IsPlaceTakenByX(board, i + (j * board_size) + 1 + (k * board_size),
+                      playing_symbol)) {
+    return 1;
+  }
+
+  return 0;
+}
+
+static int right_diagonal_checking(char *board, const char playing_symbol,
+                                   int board_size, int i, int j, int k) {
+  int right_down_jump_length = board_size - 1;
+
+  if (IsPlaceTakenByX(
+          board,
+          (i * board_size) + board_size - j + (k * right_down_jump_length),
+          playing_symbol)) {
+    return 1;
+  }
+
+  return 0;
+}
+
+static int left_diagonal_checking(char *board, const char playing_symbol,
+                                  int board_size, int i, int j, int k) {
+  int left_down_jump_length = board_size + 1;
+
+  if (IsPlaceTakenByX(board,
+                      j + (i * board_size) + 1 + (k * left_down_jump_length),
+                      playing_symbol)) {
+    return 1;
+  }
+
+  return 0;
+}
+
 static int IsSymbolOccurredXTimesAdvanced(char *board,
                                           const int number_of_players,
                                           const char playing_symbol,
-                                          const int x_times,
-                                          const int direction) {
-  int symbol_occurrence = 0;
-  const int row_and_column_size = number_of_players + 1;
+                                          const int x_times) {
+  const int board_size = number_of_players + 1;
 
-  // Checking Horizontally
-  if (direction == HORIZONTAL_DIRECTION) {
-    for (int i = 0; i < row_and_column_size - 2; ++i) {
-      for (int j = 0; j < row_and_column_size - 2; ++j) {
-        symbol_occurrence = 0;
+  int horizontal_symbol_occurrence = 0;
+  int vertical_symbol_occurrence = 0;
+  int right_diagonal_symbol_occurrence = 0;
+  int left_diagonal_symbol_occurrence = 0;
 
-        for (int k = 0; k < x_times; ++k) {
-          if (IsPlaceTakenByX(board, j + (i * row_and_column_size) + 1 + k,
-                              playing_symbol)) {
-            ++symbol_occurrence;
-          }
-        }
+  for (int i = 0; i < board_size - 2; ++i) {
+    for (int j = 0; j < board_size - 2; ++j) {
+      horizontal_symbol_occurrence = 0;
+      vertical_symbol_occurrence = 0;
+      right_diagonal_symbol_occurrence = 0;
+      left_diagonal_symbol_occurrence = 0;
 
-        if (symbol_occurrence == x_times) return i + 1;  // column number
-      }
-    }
-    return DRAW_GAME;
-  }
-
-  // Checking Vertically
-  if (direction == VERTICAL_DIRECTION) {
-    for (int i = 0; i < row_and_column_size - 2; ++i) {
-      for (int j = 0; j < row_and_column_size - 2; ++j) {
-        symbol_occurrence = 0;
-
-        for (int k = 0; k < x_times; ++k) {
-          if (IsPlaceTakenByX(
-                  board,
-                  i + (j * row_and_column_size) + 1 + (k * row_and_column_size),
-                  playing_symbol)) {
-            ++symbol_occurrence;
-          }
-        }
-
-        if (symbol_occurrence == x_times) return i + 1;  // column number
-      }
-    }
-    return DRAW_GAME;
-  }
-
-  int up_down_jump_length = row_and_column_size + 1;
-  int down_up_jump_length = row_and_column_size - 1;
-
-  // row loop
-  for (int i = 0; i < row_and_column_size - 2; ++i) {
-    // column loop
-    for (int j = 0; j < row_and_column_size - 2; ++j) {
-      symbol_occurrence = 0;
-      // X-Shape loop from Up to Down
       for (int k = 0; k < x_times; ++k) {
-        if (IsPlaceTakenByX(
-                board,
-                j + (i * row_and_column_size) + 1 + (k * up_down_jump_length),
-                playing_symbol)) {
-          ++symbol_occurrence;
-        }
+        horizontal_symbol_occurrence +=
+            horizontal_checking(board, playing_symbol, board_size, i, j, k);
+
+        vertical_symbol_occurrence +=
+            vertical_checking(board, playing_symbol, board_size, i, j, k);
+
+        right_diagonal_symbol_occurrence +=
+            right_diagonal_checking(board, playing_symbol, board_size, i, j, k);
+
+        left_diagonal_symbol_occurrence +=
+            left_diagonal_checking(board, playing_symbol, board_size, i, j, k);
       }
 
-      if (symbol_occurrence == x_times) return X_LINE_159;
+      if (horizontal_symbol_occurrence == x_times ||
+          vertical_symbol_occurrence == x_times ||
+          right_diagonal_symbol_occurrence == x_times ||
+          left_diagonal_symbol_occurrence == x_times) {
+        return i + 1;
+      }
     }
   }
 
-  // row loop
-  for (int i = 2; i < row_and_column_size; ++i) {
-    // column loop
-    for (int j = 2; j < row_and_column_size; ++j) {
-      symbol_occurrence = 0;
-      // X-Shape loop from Up to Down
-      for (int k = 0; k < x_times; ++k) {
-        if (IsPlaceTakenByX(
-                board,
-                j + (i * row_and_column_size) + 1 - (k * down_up_jump_length),
-                playing_symbol)) {
-          ++symbol_occurrence;
-        }
-      }
-
-      if (symbol_occurrence == x_times) return X_LINE_357;
-    }
-  }
-
-  // if no iteration reached X times in the targeted direction
   return DRAW_GAME;
 }
 
@@ -104,18 +101,8 @@ const char MultiplePlayerWinnerChecker(char *board, const int number_of_players,
   for (int i = 0; i < number_of_players; ++i) {
     if (isdigit(symbols[i])) continue;
 
-    if (IsSymbolOccurredXTimesAdvanced(board, number_of_players, symbols[i], 3,
-                                       HORIZONTAL_DIRECTION)) {
-      return symbols[i];
-    }
-
-    if (IsSymbolOccurredXTimesAdvanced(board, number_of_players, symbols[i], 3,
-                                       VERTICAL_DIRECTION)) {
-      return symbols[i];
-    }
-
-    if (IsSymbolOccurredXTimesAdvanced(board, number_of_players, symbols[i], 3,
-                                       X_SHAPE_DIRECTION)) {
+    if (IsSymbolOccurredXTimesAdvanced(board, number_of_players, symbols[i],
+                                       3)) {
       return symbols[i];
     }
   }
